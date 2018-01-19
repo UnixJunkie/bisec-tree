0e4b2b9db48f4f75c4dd238d24e41a421c40b323
Francois BERENGER <ligand@free.fr>
2018-01-19 09:51:52 +0900

    WIP: use more types

--- bisec_tree.ml
+++ bisec_tree.ml
@@ -16,6 +16,14 @@ module Make = functor (P: Point) -> struct
     | Very_good (* we use a heuristic to find good vp candidates *)
     | Good (* we use a cheaper heuristic to find good vp candidates *)
 
+  (* closed interval *)
+  type itv = { inf: float ;
+               sup: float }
+
+  let new_itv inf sup =
+    assert (inf <= sup);
+    { inf; sup }
+
   (* The data structure is parametrized by k:
      if there are n <= k points left, we put them
      all in the same bucket. Else, we continue constructing
@@ -23,36 +31,38 @@ module Make = functor (P: Point) -> struct
      This should save storage space and accelerate queries.
      The best value for k is probably dataset and application dependent. *)
 
-  type t = Empty
-         | Branch of
-             { (* left half-space *)
-               l_vp: P.t; (* left vantage point *)
-               l_min: float; (* min dist to l_vp in left sub tree *)
-               l_max: float; (* max dist to l_vp in left sub tree *)
-               l_over: float; (* min dist to l_vp in right sub tree *)
-               (* right half-space *)
-               r_vp: P.t; (* right vantage point *)
-               r_min: float; (* min dist to r_vp in right sub tree *)
-               r_max: float; (* max dist to r_vp in right sub tree *)
-               r_over: float; (* min dist to r_vp in left sub tree *)
-               (* sub-trees *)
-               left: t;
-               right: t }
-         | Bucket of
-             { vp: P.t; (* vantage point *)
-               dmin: float; (* min dist to vp *)
-               dmax: float; (* max dist to vp *)
-               points: P.t array } (* remaining points (vp excluded),
-                                      ordered by incr. dist. to vp. *)
+  type bucket = { vp: P.t; (* vantage point *)
+                  (* min and max dist to vp *)
+                  bounds: itv;
+                  (* remaining points (vp excluded),
+                     ordered by incr. dist. to vp. *)
+                  points: P.t array }
 
-  let new_bucket vp dmin dmax points =
-    Bucket { vp; dmin; dmax; points }
+  let new_bucket vp bounds points =
+    { vp; bounds; points }
 
-  let new_branch l_vp l_min l_max l_over r_vp r_min r_max r_over left right =
-    Branch { l_vp; l_min; l_max; l_over; r_vp; r_min; r_max; r_over; left; right }
+  type node =
+    { (* left half-space *)
+      l_vp: P.t; (* left vantage point *)
+      l_in: itv; (* dist bounds for points in the same half-space *)
+      l_out: itv; (* dist bounds for points in the other half-space *)
+      (* right half-space *)
+      r_vp: P.t; (* right vantage point *)
+      r_in: itv; (* dist bounds for points in the same half-space *)
+      r_out: itv; (* dist bounds for points in the other half-space *)
+      (* sub-trees *)
+      left: t;
+      right: t }
+  and t = Empty
+        | Node of node
+        | Bucket of bucket
+
+  let new_node l_vp l_in l_out r_vp r_in r_out left right =
+    { l_vp; l_in; l_out; r_vp; r_in; r_out; left; right }
 
   let rng = Random.State.make_self_init ()
 
+  (* n must be > 0 *)
   let rand_int n =
     Random.State.int rng n
 
@@ -65,51 +75,56 @@ module Make = functor (P: Point) -> struct
     else if x > y then 1
     else 0 (* x = y *)
 
-  (* select one vp randomly, then enrich the points with
-     their distance to this vp *)
+  (* point known by one vantage point *)
+  type point1 = { p: P.t;
+                  d1: float }
+  let point1_cmp x y =
+    float_compare x.d1 y.d1
+  (* enrich p by distance to vp *)
+  let enr (vp: P.t) (p: P.t): point1 =
+    { p; d1 = P.dist vp p }
+  (* point known by two vantage points *)
+  type point2 = { p: P.t;
+                  d1: float;
+                  d2: float }
+  let point2_cmp1 x y =
+    float_compare x.d1 y.d1
+  let point2_cmp2 x y =
+    float_compare x.d2 y.d2
+  let enr2 (vp: P.t) (p: point1): point2 =
+    { p = p.p; d1 = p.d1; d2 = P.dist vp p.p }
+
+  (* select first vp randomly, then enrich points
+     by their distance to it *)
   let rand_vp points =
     let n = Array.length points in
     assert(n > 0);
     if n = 1 then
-      [|(points.(0), 0.0)|]
+      [|{ p = points.(0); d1 = 0.0 }|]
     else
       let i = rand_int n in
       let vp = points.(i) in
-      Array.map (fun p -> (p, P.dist vp p)) points
-
-  (* enrich points with their distance to given vp *)
-  let furthest_vp vp enr_points =
-    Array.map (fun (p, d0) -> (p, d0, P.dist vp p)) enr_points
-
-  let fst3 (a, _, _) = a
-  let snd3 (_, b, _) = b
-  let trd3 (_, _, c) = c
-
-  (* pseudo double normal: we look for a double normal,
-     but we don't check we actually found one *)
-  let very_good_vp_pair points =
-    (* FBR: code this one *)
-    failwith "not implemented yet"
+      Array.map (enr vp) points
 
   (* choose one vp randomly, the furthest point from it is the other vp *)
   let good_vp_pair points =
     let n = Array.length points in
     assert(n >= 2);
     let enr_points = rand_vp points in
-    Array.sort (fun x y ->
-        float_compare (snd x) (snd y)
-      ) enr_points;
-    let vp0 = fst enr_points.(n - 1) in
-    (* remove selected vp from points array *)
-    let enr_rem = A.sub enr_points 0 (n - 1) in
-    let enr_points_2 = furthest_vp vp0 enr_rem in
-    A.sort (fun x y ->
-        float_compare (trd3 x) (trd3 y)
-      ) enr_points_2;
-    let m = A.length enr_points_2 in
-    let vp1 = fst3 enr_points_2.(m - 1) in
-    let enr_points_3 = A.sub enr_points_2 0 (m - 1) in
-    (vp0, enr_points_3, vp1)
+    Array.sort point1_cmp enr_points;
+    let vp1 = enr_points.(0).p in
+    let vp2 = enr_points.(n - 1).p in
+    (* remove selected vps from points array
+       and enrich points by their distnce to vp2 *)
+    let enr_rem = A.sub enr_points 1 (n - 2) in
+    let rem = Array.map (enr2 vp2) enr_rem in
+    (vp1, rem, vp2)
+
+  (* pseudo double normal: we look for a double normal,
+     but we don't check we actually found one *)
+  let very_good_vp_pair points =
+    (* FBR: code this one *)
+    failwith "not implemented yet"
 
   let bisect (vp0, enr_points, vp1) =
     failwith "not implemented yet"

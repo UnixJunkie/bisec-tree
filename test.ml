25c1b34ec353518b3c545a9f3f45debe03fa3424
Francois BERENGER <ligand@free.fr>
2018-01-23 18:39:54 +0900

    more tests

--- test.ml
+++ test.ml
@@ -1,5 +1,7 @@
+open Printf
 
 module A = Array
+module L = List
 
 let square x =
   x *. x
@@ -21,10 +23,48 @@ end
 
 module BST = Bisec_tree.Make (P) (C)
 
+let with_out_file fn f =
+  let output = open_out_bin fn in
+  let res = f output in
+  close_out output;
+  res
+
+let list_to_file fn to_string l =
+  with_out_file fn (fun out ->
+      L.iter (fun x -> fprintf out "%s\n" (to_string x)) l
+    )
+
+let array_to_file fn to_string a =
+  with_out_file fn (fun out ->
+      A.iter (fun x -> fprintf out "%s\n" (to_string x)) a
+    )
+
 let main () =
+  Log.color_on ();
+  Log.set_log_level Log.DEBUG;
+  (* N rand points *)
   let nb_points = 1000 in
   let points = A.init nb_points (fun _ -> P.rand ()) in
   let tree = BST.create points in
-  assert(BST.check tree)
+  (* check tree invariant *)
+  assert(BST.check tree);
+  let dists = BST.sample_distances (nb_points / 10) points in
+  let dists_fn = "test.dists" in
+  array_to_file dists_fn string_of_float dists;
+  (* check all points are in the tree *)
+  let n = L.length (BST.to_list tree) in
+  assert(n = nb_points);
+  (* test all points can be found back *)
+  assert(
+    A.for_all
+      (fun ((x, y) as p) ->
+         let found = BST.mem p tree in
+         if not found then
+           Log.error "not found: %f %f" x y;
+         found
+      ) points
+  )
+  (* neighbors *)
+  (* nearest_neighbor *)
 
 let () = main ()
